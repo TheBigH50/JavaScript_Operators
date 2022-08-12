5342acc3c7d2e08653ba1f5d9b217ec830dd128f
TheBigH50 <HFangmeier@outlook.com>
2022-08-12 11:40:40 -0500

    exe2 and log

--- app.js
+++ app.js
@@ -6,18 +6,33 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-let a = 20
-let b = 4
-let add = (a + b)
-let minus = (a - b)
-let multiply = (a * b)
-let dividing = (a / b)
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
-console.log(a, b, add, minus, multiply, dividing)
+console.log(a, b, add, minus, multiply, dividing);
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+num+str
+num+str2
+num+isPresent
+firstName+num
+isPresent+str
+firstName+lastName
+
+console.log(num+str,num+str2, num+isPresent, firstName+num, isPresent+str, firstName+lastName)
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
