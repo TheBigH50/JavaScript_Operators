4d1fe902e1b67a81286fa9211067138a9df8893e
TheBigH50 <HFangmeier@outlook.com>
2022-08-12 11:47:19 -0500

    debug

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ let isPresent2 = false;
 
 val == str3;
 val === str3;
-!isPresent2("eleven" == str4 && val >= st3)(!isPresent2 || isPresent2);
+!isPresent2;
+("eleven" == str4 && val >= st3);
+(!isPresent2 || isPresent2);
 0 == false;
 0 === false;
 0 != false;
